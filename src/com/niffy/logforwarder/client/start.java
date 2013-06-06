6baef6e240d7036b613e97d9b1d80fdae4b21ae5
Paul Robinson <niffydroid@gmail.com>
2013-06-06 13:15:30 +0100

    Now using a logging library

--- src/com/niffy/logforwarder/client/start.java
+++ src/com/niffy/logforwarder/client/start.java
@@ -1,282 +1,138 @@
 package com.niffy.logforwarder.client;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
+import java.net.InetSocketAddress;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.niffy.logforwarder.lib.ClientSelector;
+import com.niffy.logforwarder.lib.logmanagement.ILogManager;
+import com.niffy.logforwarder.lib.logmanagement.LogManagerClient;
 
 public class start {
+	private final static Logger log = LoggerFactory.getLogger(start.class);
+	public static ClientSelector CLIENT_SELECTOR;
+	public static InetSocketAddress ADDRESS;
+	public static int PORT = 1006;
+	public static int BUFFER = 20971520;
+	public static int SERVER_PORT = 1007;
+	public static ILogManager LOG_MANAGER;
+	public static int VERSION = 0;
 
-	public static NioServer mServer;
-	public static NioClient mClient;
-	public static UDPServer mUDP;
-	public static EchoWorker mWorker;
-	public static int mChoice;
-	public static InetAddress mLocalAddress;
-	public static int mLocalTCPPort;
-	public static int mLocalUDPPort;
-	public static int mRemoteTCPPort;
-	public static int mRemoteUDPPort;
-	public static InetAddress mRemoteAddress;
+	public static void main(String[] args) {
+		log.info("Started");
+		LOG_MANAGER = new LogManagerClient(VERSION);
+		ADDRESS = new InetSocketAddress(PORT);
+		try {
+			CLIENT_SELECTOR = new ClientSelector("Client Selector", ADDRESS, BUFFER, SERVER_PORT, LOG_MANAGER);
+			new Thread(CLIENT_SELECTOR).start();
+		} catch (IOException e) {
+			log.error("Error creating selector", e);
+		}
 
-	public static RspHandler handler;
-	
-	public static ArrayList<host> mHosts = new ArrayList<host>();
-
-	public static void main(String[] args) throws UnknownHostException {
-		mChoice = Integer.parseInt(args[0]);
-		mLocalAddress = InetAddress.getLocalHost();
-		mRemoteAddress = InetAddress.getLocalHost();
-
-		mWorker = new EchoWorker();
-		new Thread(mWorker).start();
-
-		if (mChoice == 0) {
-			mLocalTCPPort = Integer.parseInt(args[1]);
-			mRemoteTCPPort = Integer.parseInt(args[2]);
-			if (args.length == 4) {
-				mRemoteAddress = InetAddress.getByName(args[3]);
-				System.out.println("Using remote address: " + mRemoteAddress.getHostAddress());
-			}
+		// process();
+	}
+	/*
+		public static void process() {
+			String CurLine = ""; // Line read from standard in
+			InputStreamReader converter = new InputStreamReader(System.in);
+			BufferedReader in = new BufferedReader(converter);
 			try {
-				mServer = new NioServer(mLocalAddress, mRemoteAddress, mLocalTCPPort, mRemoteTCPPort, mWorker);
-				new Thread(mServer).start();
+				while (true) {
+					System.out.print("main Input: ");
+					CurLine = in.readLine();
+
+					if (CurLine.equals("quit")) {
+						System.exit(0);
+					} else if (CurLine.equals("tcp")) {
+						tcpCommand(in);
+					} else if (CurLine.equals("udp")) {
+						udpCommand(in);
+					}
+				}
 			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else if(mChoice == 1) {
-			mLocalTCPPort = Integer.parseInt(args[1]);
-			mRemoteTCPPort = Integer.parseInt(args[2]);
-			if (args.length == 4) {
-				mRemoteAddress = InetAddress.getByName(args[3]);
-				System.out.println("Using remote address: " + mRemoteAddress.getHostAddress());
-			}
-			try {
-				mClient = new NioClient(mRemoteAddress, mRemoteTCPPort, mLocalTCPPort);
-				new Thread(mClient).start();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else if (mChoice == 2){
-			mLocalUDPPort = Integer.parseInt(args[1]);
-			mRemoteUDPPort = Integer.parseInt(args[2]);
-			try {
-				mUDP = new UDPServer(mLocalUDPPort);
-				new Thread(mUDP).start();
-			}catch (IOException e){
+				System.out.println("error");
 				e.printStackTrace();
 			}
 		}
-		
-		handler = new RspHandler();
 
-		process();
-	}
-
-	public static void process() {
-		String CurLine = ""; // Line read from standard in
-		InputStreamReader converter = new InputStreamReader(System.in);
-		BufferedReader in = new BufferedReader(converter);
-		try {
-			while (true) {
-				System.out.print("main Input: ");
+		public static void tcpCommand(BufferedReader in) {
+			String CurLine = "";
+			try {
+				System.out.print("TCP : ");
 				CurLine = in.readLine();
-
 				if (CurLine.equals("quit")) {
 					System.exit(0);
-				} else if (CurLine.equals("tcp")) {
-					tcpCommand(in);
-				}else if (CurLine.equals("udp")){
-					udpCommand(in);
-				}
-			}
-		} catch (IOException e) {
-			System.out.println("error");
-			e.printStackTrace();
-		}
-	}
-
-	public static void tcpCommand(BufferedReader in) {
-		String CurLine = "";
-		try {
-			System.out.print("TCP : ");
-			CurLine = in.readLine();
-			if (CurLine.equals("quit")) {
-				System.exit(0);
-			} else if (CurLine.equals("connect")) {
-				connectProcess(in);
-			} else if (CurLine.equals("keys")) {
-				viewKeys();
-			} else if (CurLine.equals("send")) {
-				System.out.print("Send : ");
-				CurLine = in.readLine();
-				if (CurLine.equals("up")) {
-					return;
-				} else {
-					byte[] pBytes = CurLine.getBytes();
-					send(in, pBytes);
-					return;
-				}
-			}
-		} catch (IOException e) {
-			System.out.println("error");
-			e.printStackTrace();
-		}
-	}
-	
-	public static void udpCommand(BufferedReader in) {
-		String CurLine = "";
-		try {
-			System.out.print("UDP : ");
-			CurLine = in.readLine();
-			if (CurLine.equals("quit")) {
-				System.exit(0);
-			} else if (CurLine.equals("send")) {
-				System.out.print("Send : ");
-				CurLine = in.readLine();
-				if (CurLine.equals("up")) {
-					return;
-				} else {
-					byte[] pBytes = CurLine.getBytes();
-					sendUDP(in, pBytes);
-					return;
-				}
-			}
-		} catch (IOException e) {
-			System.out.println("error");
-			e.printStackTrace();
-		}
-	}
-
-	public static void connectProcess(BufferedReader in) {
-		String CurLine = "";
-		try {
-			System.out.print("Connect : ");
-			CurLine = in.readLine();
-			if (CurLine.equals("quit")) {
-				System.exit(0);
-			} else {
-				int port = getPort(CurLine);
-				String ip = getIP(CurLine);
-				if(port != -1){
-					System.out.println("Found: " + ip + " " + port);
-					host host = new host(ip, port);
-					mHosts.add(host);
-					connect(ip, port);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static String getIP(String pLine) {
-		int firstColon = pLine.indexOf(":");
-		firstColon++;
-		int portColon = pLine.lastIndexOf(":");
-		portColon--;
-		String found = pLine.substring(firstColon, portColon);
-		found.trim();
-		return found;
-	}
-
-	public static int getPort(String pLine) {
-		int portColon = pLine.lastIndexOf(":");
-		portColon++;
-		String found = pLine.substring(portColon);
-		found.trim();
-		try {
-			return Integer.valueOf(found);
-		} catch (NumberFormatException er) {
-			return -1;
-		}
-	}
-
-	public static void send(BufferedReader in, byte[] pBytes) throws IOException {
-		String CurLine = "";
-		try {
-			System.out.println("Send to whom? Choices : ");
-			int count = 0;
-			for (host b : mHosts) {
-				System.out.println(count + " : host op: " + b.ip + " port: " + b.port);
-				count++;
-			}
-			CurLine = in.readLine();
-			if (CurLine.equals("quit")) {
-				System.exit(0);
-			} else if (CurLine.equals("up")){
-				return;
-			}else {
-				try{
-					int index = Integer.valueOf(CurLine);
-					host b = mHosts.get(index);
-					if(b != null){
-						System.out.println("Using: host op: " + b.ip + " port: " + b.port);
-						sendTo(pBytes, b.ip, b.port);
+				} else if (CurLine.equals("connect")) {
+					connectProcess(in);
+				} else if (CurLine.equals("send")) {
+					System.out.print("Send : ");
+					CurLine = in.readLine();
+					if (CurLine.equals("up")) {
+						return;
+					} else {
+						byte[] pBytes = CurLine.getBytes();
+						send(in, pBytes);
+						return;
 					}
-				}catch (NumberFormatException e){
-					System.out.println("Not a number, going up");
-					return;
 				}
+			} catch (IOException e) {
+				System.out.println("error");
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-	}
-	
-	public static void sendTo(byte[] pBytes, String ip, int port)throws IOException{
-		if (mChoice == 0) {
-			mServer.send(mRemoteAddress.getHostAddress().toString(), pBytes);
-		} else if (mChoice == 1) {
-			mClient.send(mRemoteAddress.getHostAddress().toString(), mRemoteTCPPort, pBytes, handler);
-		} else if (mChoice == 2) {
-			mUDP.send(ip, port, pBytes);
-		}
-	}
-	
-	public static void connect(String pIP, int pPort) throws IOException {
-		if (mChoice == 0) {
-			mServer.connect(mRemoteAddress.getHostAddress().toString());
-		} else {
-			mClient.connect(pIP, pPort);
-		}
-	}
 
-	public static void viewKeys() {
-		if (mChoice == 0) {
-			mServer.viewKeys();
-		}
-	}
-
-	public static void sendUDP(BufferedReader in, byte[] pBytes) throws IOException {
-		String CurLine = "";
-		try {
-			System.out.println("Send to whom?: ");
-			CurLine = in.readLine();
-			if (CurLine.equals("quit")) {
-				System.exit(0);
-			} else if (CurLine.equals("up")){
-				return;
-			}else {
-				try{
+		public static void connectProcess(BufferedReader in) {
+			String CurLine = "";
+			try {
+				System.out.print("Connect : ");
+				CurLine = in.readLine();
+				if (CurLine.equals("quit")) {
+					System.exit(0);
+				} else {
 					int port = getPort(CurLine);
 					String ip = getIP(CurLine);
-					if(port != -1){
+					if (port != -1) {
 						System.out.println("Found: " + ip + " " + port);
-						sendTo(pBytes, ip, port);
+						host host = new host(ip, port);
+						mHosts.add(host);
+						connect(ip, port);
 					}
-				}catch (NumberFormatException e){
-					System.out.println("Not a number, going up");
-					return;
 				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-	}
+
+		public static String getIP(String pLine) {
+			int firstColon = pLine.indexOf(":");
+			firstColon++;
+			int portColon = pLine.lastIndexOf(":");
+			portColon--;
+			String found = pLine.substring(firstColon, portColon);
+			found.trim();
+			return found;
+		}
+
+		public static int getPort(String pLine) {
+			int portColon = pLine.lastIndexOf(":");
+			portColon++;
+			String found = pLine.substring(portColon);
+			found.trim();
+			try {
+				return Integer.valueOf(found);
+			} catch (NumberFormatException er) {
+				return -1;
+			}
+		}
+
+		public static void connect(String pIP, int pPort) throws IOException {
+		
+		}
+	*/
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
-	
+
 }
